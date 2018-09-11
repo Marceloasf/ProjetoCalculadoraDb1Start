1e46696326fabf51655da70e02cd5c6716c355db
marcelo <marceloasf1999@gmail.com>
2018-09-11 17:55:00 -0300

    Atualizado

--- src/main/java/br/com/db1/rest/Calculadora.java
+++ src/main/java/br/com/db1/rest/Calculadora.java
@@ -8,48 +8,29 @@ import javax.ws.rs.core.Response;
 @Path("/calculadora")
 public class Calculadora{
 	
-	private Double resultado;
-	private Double primeiroValor;
-	private Double segundoValor;
-	
-	public void setPrimeiroValor(Double primeiroValor) {
-		this.primeiroValor = primeiroValor;
-	}
-	
-	public void setSegundoValor(Double segundoValor) {
-		this.segundoValor = segundoValor;
-	}
-	
-	public Double getPrimeiroValor() {
-		return primeiroValor;
-	}
-	
-	public Double getSegundoValor() {
-		return segundoValor;
-	}
-	
+	private int resultado;
+
 	
 	@GET
 	@Path("/somar/{valor1}/{valor2}")
-	public Response somar(@PathParam("valor1") Double primeiroValor, @PathParam("valor2") Double segundoValor){
+	public Response somar(@PathParam("valor1") Integer primeiroValor, @PathParam("valor2") Integer segundoValor){
 		this.resultado = primeiroValor + segundoValor;
 		return Response.status(200).entity(resultado).build();
 	}
 	
-	public void subtrair(){
-		this.resultado = primeiroValor - segundoValor;
+	@GET
+	@Path("/subtrair/{valor1}/{valor2}")
+	public Response subtrair(@PathParam("valor1") Integer primeiroValor, @PathParam("valor2") Integer segundoValor){
+			this.resultado = primeiroValor - segundoValor;
+			return Response.status(200).entity(resultado).build();
 	}
 	
 	public void dividir(){
-		this.resultado = primeiroValor/segundoValor;
+	//	this.resultado = primeiroValor/resultado;
 	}
 	
 	public void multiplicar(){
-		this.resultado = primeiroValor * segundoValor;
-	}
-	
-	public Double exibirResultado(){
-		return resultado;
+	//	this.resultado = primeiroValor * resultado;
 	}
 	
 }
