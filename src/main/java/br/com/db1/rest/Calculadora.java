0157e42c8e954c8fc9753198c86edf0f14816ffb
marcelo <marceloasf1999@gmail.com>
2018-09-12 14:10:39 -0300

    Atualizado

--- src/main/java/br/com/db1/rest/Calculadora.java
+++ src/main/java/br/com/db1/rest/Calculadora.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.MessageBodyWriter;
 
 @Path("/calculadora")
 public class Calculadora{
